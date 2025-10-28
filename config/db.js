76d3c9ecc92d33c7a2a2003959f257dfbd2f4237
Elvin Abraham <elvinmabraham@gmail.com>
2025-10-28 13:00:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27017/housemakka");
-    console.log("✅ Database connected");
+    console.log("Database connected");
   } catch (error) {
-    console.error("❌ Database connection failed", error);
+    console.error("Database connection failed", error);
   }
 };
 
